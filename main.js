d0b7074f0668c0a75c3ffc1d0ae64e42aa7be0af
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-03-08 14:05:36 +0600

    Variables

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // Log a statement using console.log()
 console.log("Hello from main.js");
+
+// Variables
+
+let age = 25;
+console.log(age);
+
+const salary = 80000; // Initialization is required
+// salary = 50000; // Can not be reassigned
+console.log(salary);
+
+let sum; // Initialization is not required
+sum = 90;
+
+console.log(sum);
